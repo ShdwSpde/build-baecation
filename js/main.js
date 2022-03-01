14492ba6f53cb869a608f2fa405fd96f50520e8b
Mike Spade <mikespadeny@gmail.com>
2022-03-01 09:25:58 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
-document.getElementById('purple').onclick = partyPurple
-document.getElementById('green').onclick = partyGreen
-document.getElementById('blue').onclick = partyBlue
+document.getElementById('newyork').onclick = baecationNy
+document.getElementById('neworleans').onclick = baecationNo
+document.getElementById('newmexico').onclick = baecationNm
+document.getElementById('italy').onclick = baecationIt
+document.getElementById('jamaica').onclick = baecationJm
+document.getElementById('amsterdam').onclick = baecationAm
+document.getElementById('seattle').onclick = baecationSe
 
-
-function partyPurple() {
+function baecationNy() {
   document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
   document.querySelector('body').style.color = 'white'
 }
 
-function partyGreen() {
+function baecationNo() {
   document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
   document.querySelector('body').style.color = 'white'
 }
 
-function partyBlue() {
+function Nm() {
+  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function baecationIt() {
+  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function baecationAm() {
+  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function Jm() {
+  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function Se() {
   document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
   document.querySelector('body').style.color = 'white'
 }
